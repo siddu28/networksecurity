76f1bcb24f0ff83c1431f0a5662ab3b4b41f5add
siddu28 <siddukasam28@gmail.com>
2025-09-12 19:03:06 +0530

    completed Data transformation config

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -15,6 +15,7 @@ def read_yaml_file(file_path:str)->dict:
         raise NetworkSecurityException(e,sys)
     
 
+
 def write_yaml_file(file_path:str,content:object,replace:bool=False)->None:
     try:
         if replace:
@@ -24,6 +25,30 @@ def write_yaml_file(file_path:str,content:object,replace:bool=False)->None:
         os.makedirs(os.path.dirname(file_path),exist_ok=True)
         with open(file_path,"w") as file:
             yaml.dump(content,file)
+    
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+
+def save_numpy_array_data(file_path:str,array:np.array):
+
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,"wb") as file_obj:
+            np.save(file_obj,array)
+    
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+    
+
+def save_object(file_path:str,obj:object)->None:
+    try:
+        logging.info("Entered the save_object mathod of main utils class")
+        os.makedirs(os.path.dirname(file_path),exist_ok=True)
+        with open(file_path,"wb") as file_obj:
+            pickle.dump(obj,file_obj)
+        
+        logging.info("Exited the save_object method")
     
     except Exception as e:
         raise NetworkSecurityException(e,sys)
