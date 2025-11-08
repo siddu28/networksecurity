d09c390e4b986b9e7f7f709fd45cee10478912be
Sai Venkata Siddardha Kasam <157007052+siddu28@users.noreply.github.com>
2025-11-08 15:33:02 +0530

    added code to deploy model.pkl to AWS s3 bucket

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -24,9 +24,14 @@ from networksecurity.entity.artifact_entity import (
     ModelTrainerArtifact,
 )
 
+from networksecurity.constants.training_pipeline import TRAINING_BUCKET_NAME
+from networksecurity.cloud.s3_syncer import S3Sync
+from networksecurity.constants.training_pipeline import SAVED_MODEL_DIR
+
 class TrainingPipeline:
     def __init__(self):
         self.training_pipeline_config = TrainingPipelineConfig()
+        self.s3_sync = S3Sync()
 
     def start_data_ingestion(self):
         try:
@@ -93,6 +98,26 @@ class TrainingPipeline:
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
+    ## save artifacts and final model in AWS s3 bucket
+
+    ## we are storing model in cloud because if the model is huge , then it is difficult to store in memory and also we can store multiple versions of models in cloud
+    
+    ## from local artifact directory it will push to aws s3 bucket
+    def sync_artifact_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/Artifacts/{self.training_pipeline_config.timestamp}"
+            self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.artifact_dir,aws_bucket_url=aws_bucket_url)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    ## local final model is pushed to AWS s3 bucket
+    def sync_saved_model_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/final_models/{self.training_pipeline_config.timestamp}"
+            self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.model_dir,aws_bucket_url=aws_bucket_url)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
     
     def run_pipeline(self):
         try:
@@ -101,6 +126,9 @@ class TrainingPipeline:
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(data_validation_artifact)
             model_trainer_artifact = self.start_model_trainer(data_transformation_artifact)
+
+            self.sync_artifact_dir_to_s3()
+            self.sync_saved_model_dir_to_s3()
 
             return model_trainer_artifact
 
